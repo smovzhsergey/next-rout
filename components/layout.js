86875a9506ce22d9ae14b7f7cb191c8d355aed21
Smovzh Sergey <smovzh.sergey@gmail.com>
2019-12-05 09:50:23 +0200

    add google meta tag

--- components/layout.js
+++ components/layout.js
@@ -12,6 +12,7 @@ const Layout = (props) => {
 return(
   <div style={layoutStyle}>
     <Head>
+    <meta name="google-site-verification" content="C2hpjWWyK5vJL3--WZVRddwrySmQzyCGNLvXpS0Kepw" />
       <title>{"Dynamic router"}</title>
       <meta name="keywords" content={"keyword, keyword, keyword"} />
       <meta name="description" content={"Some description about current page"} />
