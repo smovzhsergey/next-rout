0e7e6e9ff67751ed172ca96dabb247e08cfa1e16
Smovzh Sergey <smovzh.sergey@gmail.com>
2019-12-05 17:45:16 +0200

    replace link

--- components/filters.js
+++ components/filters.js
@@ -4,7 +4,12 @@ import Link from 'next/link';
 export const Filters = ({ cb, selectData:[name, options], nextlink }) => {
 
   const router = useRouter();
-  const { filter1, filter2, filter3, filter4, filter5, filter6 } = router.query;
+
+  const routeIndex = Number(router.pathname.slice(-2, -1));  // (:|)
+    const { filter1, filter2, filter3, filter4, filter5, filter6 } = router.query;
+    const link =  router.pathname === '/tutors'
+    ? `[filter]/[filter2]`
+    : `${router.pathname}/[filter${routeIndex+1}]`;
   
   return (
     <div style = {{ padding: '20px' }}>
@@ -18,7 +23,7 @@ export const Filters = ({ cb, selectData:[name, options], nextlink }) => {
       {
         options.map( (option, index) => (
           <p key = {index}>
-            <Link href = { nextlink } as = {`${ router.asPath }/${ option }`}>
+            <Link href = { link } as = {`${ router.asPath }/${ option }`}>
               <a onClick = { () => cb(name, option) }>{ option }</a>
             </Link>
           </p>
