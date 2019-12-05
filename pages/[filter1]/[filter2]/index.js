bc794bcb7fe0ff296c69bcbd7467b8ad3fdf6536
Smovzh Sergey <smovzh.sergey@gmail.com>
2019-12-05 14:48:57 +0200

    router query

--- pages/[filter1]/[filter2]/index.js
+++ pages/[filter1]/[filter2]/index.js
@@ -6,6 +6,13 @@ import TutorsBoard from '../../../components/tutorsBoard'
 const Filter2 = () => {
 
   const router = useRouter()
+  const { filter1, filter2, filter3, filter4, filter5, filter6 } = router.query;
+  console.log(filter1);
+  console.log(filter2);
+  console.log(filter3);
+  console.log(filter4);
+  console.log(filter5);
+  console.log(filter6);
   
   return (
     <Layout>
