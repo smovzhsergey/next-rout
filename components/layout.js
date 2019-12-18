20274759c4a0bf6cf1d9b243d55c9ac3f48ba239
Smovzh Sergey <smovzh.sergey@gmail.com>
2019-12-18 16:04:21 +0200

    delete bundle name from Layout

--- components/layout.js
+++ components/layout.js
@@ -18,7 +18,6 @@ return(
       <meta name="description" content={"Some description about current page"} />
     </Head>
     <Header />
-    <h1>commons.c877f484ef16172d88b9.js</h1>
     {props.children}
   </div>
 )}
